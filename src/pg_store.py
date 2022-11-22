35b9172e6032a7427c9d39b9b3062b47825042c0
Rituparna sharma <sharmarituparna66@gmail.com>
2022-11-23 00:03:28 +0530

    Update pg_store.py

--- src/pg_store.py
+++ src/pg_store.py
@@ -65,7 +65,7 @@ def postgress_actions(config_path,schema_path):
     pg_container = client.containers.list()
     print('containers -->',pg_container[0].id)
     for path,name in zip(paths,tab_names):
-        p = subprocess.call(["docker", "cp", f"Data_files/feature_data/target_data/{name}.csv", f"{pg_container[0].id}:/opt/source_data"],shell=True)
+        p = subprocess.call(["docker", "cp", f"Data_files/feature_data/target_data/{name}.csv", f"{pg_container[0].id}:opt/source_data"],shell=True)
         command = f'''psql -U postgres -d monthlyaccsummary -c "\copy {name} from {path} delimiter ',' csv"'''
         subprocess.call(["docker" ,"exec" ,"t" ,f"{pg_container[0].id}" ,"/bin/bash"])
         print(pg_container[0].exec_run("ls"))
